33e35d0febd6181e4af202615da773ea2131edeb
Abrar2003 <spartanabrar@gmail.com>
2023-09-29 19:34:55 +0530

    intial commit

---
+++ masai-cp-problems-2230-1647-poGxeQ-7726d54007a0a2409659a1f38bc172349e860461/src/Components/BillPage.jsx
@@ -0,0 +1,14 @@
+import LeftBox from "./LeftBox";
+import RightBox from "./RightBox";
+
+const BillPage = () => {
+  return (
+    <div className="billbox">
+      {/* Add LeftBox and RightBox component here */}
+      <LeftBox />
+      <RightBox />
+    </div>
+  );
+};
+
+export default BillPage;
